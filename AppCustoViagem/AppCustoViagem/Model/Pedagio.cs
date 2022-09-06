fe3babf06dbcced6dd86f6528c9820141a589b8a
Usuario <Usuario@CPD02-M05>
2022-09-06 12:16:26 -0300

    Adicionar arquivos de projeto.

---
+++ AppCustoViagem/AppCustoViagem/Model/Pedagio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppCustoViagem.Model
+{
+    public class Pedagio
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Localizacao { get; set; }
+        public double Valor { get; set; }
+      
+    }
+}
---
+++ AppCustoViagem/AppCustoViagem/Model/Viagem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppCustoViagem.Model
+{
+    public class Viagem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public double Distancia { get; set; }
+        public double Consumo { get; set; }
+        public double PrecoCombustivel { get; set; }
+    }
+}
